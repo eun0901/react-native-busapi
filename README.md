c6c4e689db0b2a9091502fce9e3877f90fd2c6cf
eun0901 <jk2310400@ajou.ac.kr>
2022-09-15 01:07:31 +0900

    9/15

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+npm install
+
 npm install styled-components prop-types
 
 npm install react-native-html-parser
--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
 import React, { useState } from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Dimensions} from 'react-native';
 import SearchBox from './components/SearchBox';
 import { DOMParser } from 'xmldom';
+import styled from 'styled-components/native';
+import StationList from './StationList';
 
 const App = () => {
-    const [word, setWord] = useState('');
-    const [station, setStation] = useState({});
+    const [station, setStation] = useState('');
+    const [result, setResult] = useState([]);
+    const width = Dimensions.get('window').width;
 
     const _handleTextChange = text => {
-        setWord(text);
+        setStation(text);
     };
 
-    const _addStation = async(e)=>{
+    const _addStation = async(e) =>{
           try{
             var xhr = new XMLHttpRequest();
             const API_KEY = 'UkgvlYP2LDE6M%2Blz55Fb0XVdmswp%2Fh8uAUZEzUbby3OYNo80KGGV1wtqyFG5IY0uwwF0LtSDR%2FIwPGVRJCnPyw%3D%3D';
             const url = 'http://apis.data.go.kr/6410000/busstationservice/getBusStationList'; /*URL*/
-            const result = encodeURIComponent(word);
-            var queryParams = `${url}?serviceKey=${API_KEY}&keyword=${word}`;
+            var queryParams = `${url}?serviceKey=${API_KEY}&keyword=${station}`;
             xhr.open('GET', queryParams);
             xhr.onreadystatechange = function () {
               if (this.readyState == 4) {
                 console.log(this.responseText);
-                var xmlDoc = new DOMParser().parseFromString(this.responseText, 'text/xml');
-                console.log("success?");
-                let value = xmlDoc.getElementsByTagName("stationName")[0].textContent;
-                console.log(value);
-                setStation(value);
+                let xmlDoc = new DOMParser().parseFromString(this.responseText, 'text/xml');
+                let i = 0;
+
+                while(1){
+                  var tmpnode = new Object();
+                  
+                  tmpnode.id = xmlDoc.getElementsByTagName("stationId")[i].textContent;
+
+                  tmpnode.name = xmlDoc.getElementsByTagName("stationName")[i].textContent;
+
+                  tmpnode.x = xmlDoc.getElementsByTagName("x")[i].textContent;
+            
+                  tmpnode.y = xmlDoc.getElementsByTagName("y")[i].textContent;
+                  
+                  if(tmpnode.id == undefined) break;
+
+                  result.push(tmpnode);
+                  console.log(result);
+
+                  setResult(result);
+                  i++;
+                  console.log(result);
+                }
               }
             }
           xhr.send('');
@@ -37,7 +57,7 @@ const App = () => {
       };
 
     const _onBlur = () => {
-        setWord('');
+        setStation('');
     };
 
 
@@ -47,15 +67,20 @@ const App = () => {
             <Text style={styles.title}>Bus api</Text>
             <SearchBox 
             placeholder="정류장 이름을 입력하세요"
-            value={word}
+            value={station}
             onChangeText={_handleTextChange}
             onSubmitEditing={_addStation}
             onBlur={_onBlur}
             />
-            {
-              Object.keys(station).length !== 0 && (
-                <Text className="stationName">{station}</Text>
-              )}
+            <List width={width}>
+            {result.map(item => {
+              return(
+                <View>
+              <StationList item={item}/>
+              </View>
+              )
+            })}
+            </List>
         </View>
     );
 };
@@ -72,5 +97,10 @@ const styles = StyleSheet.create({
         margin: 10,
     },
 });
+
+const List = styled.ScrollView`
+flex: 1;
+width: ${({width})=> width - 40}px;
+`;
 
 export default App;
